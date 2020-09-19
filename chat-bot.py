45f2168cd187c14537187de0f74550c311db9ad9
daniel <duque1daniel@gmail.com>
2020-09-19 14:19:36 -0700

    Added bot responses

--- chat-bot.py
+++ chat-bot.py
@@ -3,3 +3,9 @@ from random import choice
 print("Welcome to voter bot!")
 print("I will help you with steps on how to register, check if you're registered or update your current registration")
 print("You may press done to exit the program at anytime.")
+
+def get_vote_bot_response(user_response):
+
+  bot_response_YES = ["Great! You can update your registration at https://www.vote.org/ if you need to."]
+  bot_response_NO = ["Please head to https://www.vote.org/ to register."]
+  bot_response_IDK = ["No problem! Please head to https://www.vote.org/ to check if you are currently registered."]
