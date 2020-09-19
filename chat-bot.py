16346f00ff013128c771aef2066cd62727af7de8
daniel <duque1daniel@gmail.com>
2020-09-19 15:33:48 -0700

    final fixes program running

--- chat-bot.py
+++ chat-bot.py
@@ -1,15 +1,17 @@
-from random import choice
+from random import choice #import will give access to random module/library - choice will randomly return an element in list
 #Welcome to Vote Bot
 print("Welcome to voter bot!")
 print("I will help you with steps on how to register, check if you're registered or update your current registration.")
 print("You may press done to exit the program at anytime.")
-#Function that responds to user input by the bot
+#Function that responds to user input
 def get_vote_bot_response(user_response):
 
   bot_response_YES = ["Great! You can update your registration at https://www.vote.org/ if you want to."]
   bot_response_NO = ["Please head to https://www.vote.org/register-to-vote/ to register."]
   bot_response_IDK = ["No problem! Please go to https://www.vote.org/am-i-registered-to-vote/ to check if you are currently registered."]
-#Takes user input and tie it to the correct bot reponse above
+
+#Takes user input and ties it to the correct bot reponse above
+
   if user_response == "YES":
     return choice(bot_response_YES)
   elif user_response == "NO":
@@ -20,12 +22,17 @@ def get_vote_bot_response(user_response):
     return "Uh.. I didn't quite understand that."
 
 user_response = ""
+
 #Loop that keeps the function running until the user inputs 'done'
+
 while True:
+
   user_response = input("Are you registered to vote? (YES/NO/IDK): ").upper()
+
 #Quits the program if the user inputs 'done'
+
   if user_response == 'done'.upper():
       break
-
+#Prints bot and user response
   bot_response = get_vote_bot_response(user_response)
   print(bot_response)
