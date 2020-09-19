4e7f9486da0eca14b7a2414823b048b8b6ee9a0b
daniel <duque1daniel@gmail.com>
2020-09-19 14:18:14 -0700

    Initial set up

---
+++ chat-bot.py
@@ -0,0 +1,5 @@
+from random import choice
+
+print("Welcome to voter bot!")
+print("I will help you with steps on how to register, check if you're registered or update your current registration")
+print("You may press done to exit the program at anytime.")
